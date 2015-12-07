b2b1b2fb9ee24ab2dc1c8aa63e766eb8ae98f8e5
Stephen Barlow <rebelboy11@live.com>
2015-12-07 16:19:13 -0600

    Add better music system
    This updates my previous music system quite a bit. This version chooses
    a random song from a youtube playlist, giving us dynamic control over
    the music. Previously, we would have to code in each song we want to
    use.

--- dev/js/music.js
+++ dev/js/music.js
@@ -1,10 +1,26 @@
-document.getElementById('musicplayer').addEventListener("ended",function(){
-updatemusic();
-});
+function onGoogleLoad() {
+            gapi.client.setApiKey('AIzaSyBTEZZpbrLR-DQfbbWiKNz33Tyf3zpjGes');
+            gapi.client.load('youtube', 'v3', function() {
+
+                var request = gapi.client.youtube.playlistItems.list({
+                    part: 'snippet',
+                    playlistId: 'PLP9bTe3zpmVmFKcb0u7v9ROYjV_21JTsF',
+                    maxResults: 50
+                });
+
+                request.execute(function(response) {
+                    for (var i = 0; i < response.items.length; i++) {
+						songkeys[i] = response.items[i].snippet.resourceId.videoId;
+						songnames[i] = response.items[i].snippet.title;
+                    }
+					updatemusic();
+                });
+            });
+}
+var songkeys = [];
+var songnames = [];
 function updatemusic()
 {
-var songkeys = ["p7ZsBPK656s","x_OwcYTNbHs"];
-var songnames = ["Disfigure - Blank","Jim Yosef - Firefly"];
 var song = Math.floor(Math.random()*songkeys.length);
 document.getElementById('audioplayer').innerHTML = songnames[song]+"<audio id='musicplayer' controls autoplay><source src='http://youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v="+songkeys[song]+"' type='audio/mpeg'></audio>'";
 document.getElementById('musicplayer').addEventListener("ended",function(){
