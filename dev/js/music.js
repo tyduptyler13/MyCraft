b197de8c8e8adc73149ea11cbb6f2d67603fc8bd
Stephen Barlow <rebelboy11@live.com>
2015-12-07 21:31:34 -0600

    Music can now get ALL videos in a playlist
    This update allows the player to get every video in YouTube's paginated
    API. Currently we use NoCopyrightSound's "All Releases" playlist.

--- dev/js/music.js
+++ dev/js/music.js
@@ -1,29 +1,41 @@
+var results = [];
 function onGoogleLoad() {
-            gapi.client.setApiKey('AIzaSyBTEZZpbrLR-DQfbbWiKNz33Tyf3zpjGes');
-            gapi.client.load('youtube', 'v3', function() {
+    gapi.client.setApiKey('AIzaSyBTEZZpbrLR-DQfbbWiKNz33Tyf3zpjGes');
+    gapi.client.load('youtube', 'v3', function() {
 
-                var request = gapi.client.youtube.playlistItems.list({
-                    part: 'snippet',
-                    playlistId: 'PLP9bTe3zpmVmFKcb0u7v9ROYjV_21JTsF',
-                    maxResults: 50
-                });
+        loadlist("", function() {
+            for (var i = 0; i < results.length; i++) {
+                songkeys[i] = results[i].snippet.resourceId.videoId;
+                songnames[i] = results[i].snippet.title;
+            }
+            updatemusic();
+        });
 
-                request.execute(function(response) {
-                    for (var i = 0; i < response.items.length; i++) {
-						songkeys[i] = response.items[i].snippet.resourceId.videoId;
-						songnames[i] = response.items[i].snippet.title;
-                    }
-					updatemusic();
-                });
-            });
+    });
+}
+function loadlist(token, finished) {
+
+    var request = gapi.client.youtube.playlistItems.list({
+        part: 'snippet',
+        playlistId: 'PLRBp0Fe2GpgnIh0AiYKh7o7HnYAej-5ph',
+        maxResults: 50,
+        pageToken: token
+    });
+
+    request.execute(function(response) {
+        results = results.concat(response.items);
+        if (!response.nextPageToken)
+            finished();
+        else
+            loadlist(response.nextPageToken, finished);
+    });
 }
 var songkeys = [];
 var songnames = [];
-function updatemusic()
-{
-var song = Math.floor(Math.random()*songkeys.length);
-document.getElementById('audioplayer').innerHTML = songnames[song]+"<audio id='musicplayer' controls autoplay><source src='http://youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v="+songkeys[song]+"' type='audio/mpeg'></audio>'";
-document.getElementById('musicplayer').addEventListener("ended",function(){
-updatemusic();
-});
+function updatemusic() {
+    var song = Math.floor(Math.random() * songkeys.length);
+    document.getElementById('audioplayer').innerHTML = "<p>" + songnames[song] + "</p></img><audio id='musicplayer' controls autoplay><source src='http://youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v=" + songkeys[song] + "' type='audio/mpeg'></audio><button onclick='updatemusic();'>></button>";
+    document.getElementById('musicplayer').addEventListener("ended", function() {
+        updatemusic();
+    });
 }
