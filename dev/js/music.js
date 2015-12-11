92ebf3eb2819a56f73323b14e5fe033cea20b6cf
Stephen Barlow <rebelboy11@live.com>
2015-12-10 22:23:49 -0600

    Changed the music player to pure YouTube
    We download the playlist data and info from the API, then use the
    Player_API to play the video. The video runs in margins of 0x0, so it is
    invisible. There is a Pause/Play and a Skip button provided in the UI.

--- dev/js/music.js
+++ dev/js/music.js
@@ -1,9 +1,10 @@
 var results = [];
+
 function onGoogleLoad() {
-	
-	document.getElementById('audioplayer').innerHTML = "<button onclick='startmusic();'><i class='fa fa-music'></i></button>";
-	document.getElementById('musicTitle').innerHTML = "You can play music by clicking the note in the bottom right corner.";
-	$("#musicTitle").delay(4000).fadeOut(1000);
+
+    document.getElementById('audioplayer').innerHTML = "<button onclick='startmusic();'><i class='fa fa-music'></i></button>";
+    document.getElementById('musicTitle').innerHTML = "You can play music by clicking the note in the bottom right corner.";
+    $("#musicTitle").delay(4000).fadeOut(1000);
 }
 
 function loadlist(token, finished) {
@@ -25,19 +26,28 @@ function loadlist(token, finished) {
 }
 var songkeys = [];
 var songnames = [];
+var ytplayer;
 
 function updatemusic() {
     var song = Math.floor(Math.random() * songkeys.length);
-    document.getElementById('audioplayer').innerHTML = "<i class='fa fa-music'></i> Current Song: " + songnames[song] + " <audio id='musicplayer' controls autoplay><source src='http://youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v=" + songkeys[song] + "' type='audio/mpeg'></audio><button onclick='updatemusic();'><i class='fa fa-step-forward'></i></button>";
-    document.getElementById('musicplayer').addEventListener("ended", function() {
-        updatemusic();
+    document.getElementById('audioplayer').innerHTML = "<i class='fa fa-music'></i> <span style='opacity: 1.0;'> Current Song: " + songnames[song] + " </span><div id='ytplayer'></div><button style='opacity: 1.0;background-color: transparent;color: white;' id='controlbutton' onclick='toggleMusic();'><i class='fa fa-pause'></i></button></div><button style='opacity: 1.0;background-color: transparent;color: white;' onclick='updatemusic();'><i class='fa fa-step-forward'></i></button>";
+    ytplayer = new YT.Player('ytplayer', {
+        height: '0',
+        width: '0',
+        videoId: songkeys[song],
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
     });
-	document.getElementById('musicTitle').innerHTML = songnames[song];
-	$("#musicTitle").fadeIn(200).delay(10000).fadeOut(200);
+
+
+    document.getElementById('musicTitle').innerHTML = songnames[song];
+    $("#musicTitle").fadeIn(200).delay(10000).fadeOut(200);
 }
 
-function startmusic(){
-	gapi.client.setApiKey('AIzaSyBTEZZpbrLR-DQfbbWiKNz33Tyf3zpjGes');
+function startmusic() {
+    gapi.client.setApiKey('AIzaSyBTEZZpbrLR-DQfbbWiKNz33Tyf3zpjGes');
     gapi.client.load('youtube', 'v3', function() {
 
         loadlist("", function() {
@@ -53,4 +63,37 @@ function startmusic(){
         });
 
     });
+}
+
+var isplayingmusic = false;
+
+function onYouTubePlayerAPIReady() {
+    updatemusic();
+}
+
+var eventtarget;
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+    eventtarget = event.target;
+}
+
+function onPlayerStateChange(event) {
+    if (event.data === 0) {
+        updatemusic();
+    } else if (event.data == 1) {
+        isplayingmusic = true;
+    } else if (event.data == 2) {
+        isplayingmusic = false;
+    }
+}
+
+function toggleMusic() {
+    if (isplayingmusic) {
+        eventtarget.pauseVideo();
+        document.getElementById('controlbutton').innerHTML = "<i class='fa fa-play'></i>";
+    } else {
+        eventtarget.playVideo();
+        document.getElementById('controlbutton').innerHTML = "<i class='fa fa-pause'></i>";
+    }
 }
